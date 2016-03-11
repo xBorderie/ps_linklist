74a48b9163afd77d599997fef2892353f9946fd9
Maxime Biloé <maxime.biloe@gmail.com>
2016-03-11 17:19:48 +0100

    // Add Stores link

--- src/LinkBlockRepository.php
+++ src/LinkBlockRepository.php
@@ -213,6 +213,7 @@ class LinkBlockRepository
         $staticPages = [
             'contact',
             'sitemap',
+            'stores',
         ];
 
         foreach ($staticPages as $staticPage) {
